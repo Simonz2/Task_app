f64ba3ab56ad8bff58c1cccae2b086555e6f7ee9
Simonz2 <szapatan29@gmail.com>
2025-08-06 12:10:43 -0500

    fix: manage env variables

--- main.go
+++ main.go
@@ -7,26 +7,35 @@ import (
 
 	"github.com/Simonz2/Task_app/pkg/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("now connected to DB todos")
 	app := fiber.New()
-	app.Use(cors.New(cors.Config{
+	/*app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:5173",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
+	*/
+	if os.Getenv("ENV") != "production" {
+		//load the .env file if not in production
+		_ = godotenv.Load(".env")
+		/*
+			if err != nil {
+				log.Fatal("Error loading .env file")
+			}
+		*/
+	}
+
 	if os.Getenv("ENV") == "production" {
 		app.Static("/", "./client/dist")
 	}
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "5000" //default port
+	}
 	routes.RegisterTodoRoutes(app)
 	log.Fatal(app.Listen(":" + PORT))
 }
